cec307ad30fca47980764295c64080ae55026fd1
Lembani Sakala <lembanisakala@gmail.com>
2022-04-28 05:40:25 +0200

    Update script to add tasks[]

--- src/index.js
+++ src/index.js
@@ -2,11 +2,33 @@ import _ from 'lodash';
 import './style.css';
 
 function component() {
-  const element = document.createElement('div');
+  const tasks = [
+    {
+      description: 'Make breakfast.',
+      completed: true,
+      index: 0,
+    },
+    {
+      description: 'Do morning workout.',
+      completed: false,
+      index: 1,
+    },
+    {
+      description: 'Prepare for morning session.',
+      completed: true,
+      index: 2,
+    },
+    {
+      description: 'Join morning session meeting.',
+      completed: false,
+      index: 3,
+    },
+  ];
 
-  element.innerHTML = _.join(['Hello', 'To Do List'], ' ');
+  const mainHeading = document.querySelector('.main-heading');
+  mainHeading.innerHTML = _.join(['To Do List'], ' ');
 
-  return element;
+  return mainHeading;
 }
 
 document.body.appendChild(component());
