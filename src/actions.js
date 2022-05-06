4425b06877d0b36ce03df458fc4e45daf6265801
Lembani Sakala <lembanisakala@gmail.com>
2022-05-06 11:30:08 +0200

    Update to refactor all methos from being static

--- src/actions.js
+++ src/actions.js
@@ -1,10 +1,7 @@
-const { taskForm } = document.forms;
-const { description } = taskForm;
-
 const mainTasks = document.querySelector('.main-tasks');
 
 export default class Task {
-  static showTasks = () => {
+  showTasks = () => {
     this.tasks = [];
     this.tasks = JSON.parse(localStorage.getItem('tasks')) !== null ? (this.tasks = JSON.parse(localStorage.getItem('tasks'))) : [];
     mainTasks.innerHTML = '';
@@ -12,21 +9,21 @@ export default class Task {
       let taskTemplate = `<div class="tasks-container" id="${task.index}">`;
       if (task.completed) {
         taskTemplate += `
-                  <span class="material-icons check" id="${task.index}">
+                  <span class="material-icons check">
                     done
                   </span>
-                  <input type="text" class="description completed" id="${task.index}" value="${task.description}">
-                  <span class="edit-icon material-icons">delete<span>
+                  <input type="text" class="description completed" value="${task.description}">
+                  <span class="edit-icon material-icons id="del">delete<span>
                   </div>
                   <hr>
                   `;
       } else {
         taskTemplate += `
-                  <span class="material-icons check" id="${task.index}">
+                  <span class="material-icons check">
                     check_box_outline_blank
                   </span>
-                  <input type="text" class="description" id="${task.index}" value="${task.description}">
-                  <span class="edit-icon material-icons">delete<span>
+                  <input type="text" class="description" value="${task.description}">
+                  <span class="edit-icon material-icons" id="del">delete<span>
                   </div>
                   <hr>
                   `;
@@ -37,9 +34,7 @@ export default class Task {
     this.actions();
   };
 
-  static addTask = (e) => {
-    e.preventDefault();
-
+  addTask = (descrip) => {
     this.tasks = [];
     this.tasks = JSON.parse(localStorage.getItem('tasks')) !== null ? (this.tasks = JSON.parse(localStorage.getItem('tasks'))) : [];
 
@@ -49,41 +44,38 @@ export default class Task {
       index: 0,
     };
 
-    if (description.value === '') {
+    if (descrip.value === '') {
       return false;
     }
 
-    task.description = description.value;
+    task.description = descrip.value;
     task.index = this.tasks.length + 1;
     this.tasks.push(task);
     localStorage.setItem('tasks', JSON.stringify(this.tasks));
-    description.value = '';
+    descrip.value = '';
     this.showTasks();
     return true;
   };
 
-  static deleteTask = (e) => {
+  deleteTask = (id) => {
     this.tasks = [];
     this.tasks = JSON.parse(localStorage.getItem('tasks')) !== null ? (this.tasks = JSON.parse(localStorage.getItem('tasks'))) : [];
 
-    if (e.target.classList.contains('edit-icon')) {
-      const id = Number(e.target.parentNode.attributes.id.value);
-      this.tasks.splice((id - 1), 1);
-      this.tasks.forEach((task, index) => {
-        task.index = index + 1;
-      });
-      this.tasks.sort((x, y) => x.index - y.index);
-      localStorage.setItem('tasks', JSON.stringify(this.tasks));
-      this.showTasks();
-    }
+    this.tasks.splice((id - 1), 1);
+    this.tasks.forEach((task, index) => {
+      task.index = index + 1;
+    });
+    this.tasks.sort((x, y) => x.index - y.index);
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    this.showTasks();
   };
 
-  static editTask = (index, description) => {
+  editTask = (index, description) => {
     this.tasks[index - 1].description = description;
     localStorage.setItem('tasks', JSON.stringify(this.tasks));
   };
 
-  static updateStatus(index) {
+  updateStatus(index) {
     if (index !== undefined) {
       if (this.tasks[index - 1].completed === true) {
         this.tasks[index - 1].completed = false;
@@ -96,18 +88,22 @@ export default class Task {
     this.showTasks();
   }
 
-  static clearComplete = () => {
+  clearComplete = () => {
     this.tasks = this.tasks.filter((task) => task.completed !== true);
+    this.tasks.forEach((task, index) => {
+      task.index = index + 1;
+    });
+    this.tasks.sort((x, y) => x.index - y.index);
     localStorage.setItem('tasks', JSON.stringify(this.tasks));
     this.showTasks();
   }
 
-  static actions = () => {
+  actions = () => {
     const allTasks = document.querySelectorAll('.tasks-container');
     if (allTasks) {
       allTasks.forEach((vTask) => {
         vTask.addEventListener('change', (e) => {
-          const index = e.target.getAttribute('id');
+          const index = vTask.id;
           const description = e.target.value;
           this.editTask(index, description);
           this.showTasks();
@@ -117,8 +113,9 @@ export default class Task {
     const statusBtns = document.querySelectorAll('.check');
     if (statusBtns !== null) {
       statusBtns.forEach((checkBtn) => {
-        checkBtn.addEventListener('click', (e) => {
-          this.updateStatus(e.target.attributes.id.value);
+        checkBtn.addEventListener('click', () => {
+          const checkId = checkBtn.parentNode.id;
+          this.updateStatus(checkId);
         });
       });
     }
